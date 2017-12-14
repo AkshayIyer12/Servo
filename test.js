1b78490b7f24099669036dbf9fa639f10c382618
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-12-14 14:28:42 +0530

    Test res.send & res.setContentType

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
-let {fireServer, addRoutes} = require('./server')
-fireServer(3000)
-addRoutes('GET', '/', (req, res) => {
-  res.body = 'Yo'
+let {startServer, addRoutes} = require('./server')
+startServer(3000)
+addRoutes('GET', '/index', (req, res) => {
+  console.log('im here')
+  res.setContentType('./index.txt')
+  res.send('Servo !!!')
 })
