cc809391ee543090b7d9d010bb0cceb70ae18b8d
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-12-15 08:32:29 +0530

    Send hello world

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 let {startServer, addRoutes} = require('./server')
 startServer(3000)
+// addStaticFile()
 addRoutes('GET', '/index', (req, res) => {
   console.log('im here')
-  res.setContentType('./index.txt')
-  res.send('Servo !!!')
+  res.send('Hello World')
 })
