7aee87555318b9e7bbd8bfd9c111b0c7dd9c082d
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-12-15 12:49:05 +0530

    Add setTimeout for sockets

--- server.js
+++ server.js
@@ -1,76 +1,22 @@
 const net = require('net')
-const Response = require('./response')
-const routes = {
-  GET: {},
-  POST: {}
-}
-const startServer = port => {
+// const {createResponse} = require('./response')
+// const {createRequest} = require('./request')
+// let connectionHandler = null
+
+const createServer = port => {
   let server = net.createServer(socket => {
     console.log('client connected')
     socket.on('end', () => console.log('Client Disconnected'))
-    socket.on('data', data => createResponse(parseRequest(data), socket))
+    socket.on('data', data => console.log(data.toString()))
+    socket.setTimeout(60000)
+    socket.on('timeout', () => {
+      console.log('Socket Timeout')
+      socket.end()
+    })
   })
   server.on('error', err => { throw err })
   server.listen(port, () => console.log(`Server bound on ${port}`))
 }
-
-const parseRequest = data => {
-  let message = {}
-  let newData = data.toString('utf-8')
-  let [rawStatusLine, rawHeaders] = separateStatusHeader(newData)
-  let {method, version, URI} = parseStatusLine(rawStatusLine)
-  let header = parseHeaders(rawHeaders)
-  message.method = method
-  message.httpVersion = version
-  message.headers = header
-  message.url = URI
-  return message
-}
-
-const separateStatusHeader = data => {
-  let statusLineEnd = data.indexOf('\r\n')
-  let headerLineEnd = data.indexOf('\r\n\r\n')
-  let rawStatusLine = data.slice(0, statusLineEnd)
-  let rawHeaders = data.slice(statusLineEnd + 1, headerLineEnd)
-  return [rawStatusLine, rawHeaders]
-}
-
-const parseHeaders = headers => {
-  let arr = headers.trim().split('\r\n')
-  let header = {}
-  for (let key of arr) {
-    let findColon = key.indexOf(':')
-    header[key.slice(0, findColon).toLowerCase()] = key.slice(findColon + 1,
-      key.length).trim()
-  }
-  return header
-}
-
-const parseStatusLine = data => {
-  let statusLine = data.split(' ')
-  let method = statusLine[0]
-  let URI = statusLine[1]
-  let version = statusLine[2].split('/')[1]
-  return {
-    method,
-    URI,
-    version
-  }
-}
-
-const createResponse = (req, socket) => {
-  let res = new Response(socket)
-  if (req.method === 'GET') {
-    routes[req.method][req.url](req, res)
-  }
-}
-
-const addRoutes = (method, route, cb) => {
-  routes[method][route] = cb
-  return routes
-}
-
 module.exports = {
-  startServer,
-  addRoutes
+  createServer
 }
