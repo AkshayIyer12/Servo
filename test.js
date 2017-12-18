c1b2eef7661830c27a588ee2ee60aa91e99d8622
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-12-18 15:08:38 +0530

    Update tests

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
 let http = require('./server')
-http.createServer(3000)
+http.createServer(4000, (req, res) => {
+  console.log(req, res)
+  res.write('<h1>Geekskool</h1>')
+  res.write('<p>Ping!!!</p>')
+  res.end()
+})
