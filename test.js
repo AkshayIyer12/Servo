5f65b0ecfe6166ceea4bd7645c5ebf587c1636d7
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-12-19 11:39:33 +0530

    Test get and post

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
 let http = require('./server')
-http.createServer(4000, (req, res) => {
-  console.log(req, res)
+http.createServer(4000)
+http.addRoutes('GET', '/', (req, res) => {
   res.write('<h1>Geekskool</h1>')
   res.write('<p>Ping!!!</p>')
+  res.end()
+})
+http.addRoutes('POST', '/', (req, res) => {
+  res.write('Hello Geekskool, there is a hackathon this saturday')
+  res.setHeader('Content-Type', 'text/plain')
+  res.end()
+})
+http.addRoutes('GET', '/user', (req, res) => {
+  res.write(JSON.stringify({status: 'error', message: 'yo'}))
+  res.end()
+})
+http.addRoutes('POST', '/user', (req, res) => {
+  res.write('Data Received!!!')
+  console.log(req.body)
+  res.setHeader('Content-Type', 'text/plain')
   res.end()
 })
