50fe7cb5c3c3e5686322ac69d70af2833222f2f2
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-12-20 08:47:31 +0530

    Change error message & remove console.log

--- test.js
+++ test.js
@@ -1,22 +1,25 @@
 let http = require('./server')
 http.createServer(4000)
+
 http.addRoutes('GET', '/', (req, res) => {
   res.write('<h1>Geekskool</h1>')
-  res.write('<p>Ping!!!</p>')
+  res.write('<p>Ping!!! You made a GET Request</p>')
   res.end()
 })
+
 http.addRoutes('POST', '/', (req, res) => {
   res.write('Hello Geekskool, there is a hackathon this saturday')
   res.setHeader('Content-Type', 'text/plain')
   res.end()
 })
+
 http.addRoutes('GET', '/user', (req, res) => {
-  res.write(JSON.stringify({status: 'error', message: 'yo'}))
+  res.write(JSON.stringify({status: 'error', message: 'Error Message'}))
   res.end()
 })
+
 http.addRoutes('POST', '/user', (req, res) => {
   res.write('Data Received!!!')
-  console.log(req.body)
   res.setHeader('Content-Type', 'text/plain')
   res.end()
 })
