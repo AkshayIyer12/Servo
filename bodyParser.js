ba5882cd9a0792c8b25ede166fd53ae6851337bb
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-12-21 03:08:52 +0530

    Add checks for the parsers & add multipart parser

--- bodyParser.js
+++ bodyParser.js
@@ -1,47 +1,82 @@
+const compose = (f, g) => x => f(g(x))
 
-const parseJSON = (req, res) => {
-  req.body = JSON.parse(req.body.toString())
-  return [req, res]
+const parseJSON = req => {
+  if (req.headers['Content-Type'] === 'application/json') {
+    req.body = JSON.parse(req.body.toString())
+    return req
+  }
+  return null
 }
 
-const parsePlainText = (req, res) => {
-  req.body = req.body.toString()
-  return [res, res]
+const parsePlainText = req => {
+  if (req.headers['Content-Type'] === 'text/plain') {
+    req.body = req.body.toString()
+    return req
+  }
+  return null
 }
 
-const parseURLEncoded = (req, res) => {
-  let data = req.body.toString().split('&').reduce((accum, v) => {
-    let val = v.split('=')
-    accum[val[0]] = val[1]
+const parseURLEncoded = req => {
+  if (req.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+    let data = req.body.toString().split('&').reduce((accum, v) => {
+      let val = v.split('=')
+      accum[val[0]] = val[1]
+      return accum
+    }, {})
+    req.body = data
+    return req
+  }
+  return null
+}
+
+const parseMultipart = req => {
+  if (req.headers['Content-Type'].slice(0, 19) === 'multipart/form-data') {
+    let [body, files] = compose(parseParts, getParts)(req)
+    req.body = body
+    req.files = files
+    return req
+  }
+  return null
+}
+
+const getParts = req => {
+  let index = req.headers['Content-Type'].indexOf('=')
+  let boundary = req.headers['Content-Type'].slice(index + 1)
+  let tempBody = req.body
+  let arr = []
+  while (tempBody.indexOf('--' + boundary + '--') > 0) {
+    let chunk = tempBody.slice(boundary.length + 4, tempBody.indexOf('--' + boundary, boundary.length + 4))
+    arr.push(chunk)
+    tempBody = tempBody.slice(boundary.length + 4 + chunk.length)
+  }
+  return [arr, boundary]
+}
+
+const parseParts = ([arr, boundary]) => {
+  let files = {}, fname = ''
+  let parsedBody = arr.reduce((accum, a) => {
+    let header = a.slice(0, a.indexOf('\r\n\r\n')).toString('utf-8').split('\r\n')
+    let body = a.slice(a.indexOf('\r\n\r\n') + 4)
+    if (header[0].includes('filename')) {
+      [files, fname] = getKeyValue(header[0], body)
+      accum.filenames.push(fname)
+    } else {
+      let key = header[0].slice(header[0].indexOf('=') + 1)
+      accum[key.slice(1, key.length - 1)] = body
+    }
     return accum
-  }, {})
-  req.body = data
-  return [req, res]
+  }, {
+    filenames: []
+  })
+  return [parsedBody, files]
 }
 
-const parseMultipart = (req, res) => {
-  let boundary = req.headers['Content-Type']
-  .slice(21, req.headers['Content-Type'].length)
-  .split('=')[1]
-  let splitter = req.body.split('--' + boundary + '\r\n')
-  let newSplitter = splitter
-  .slice(1)
-  .map(a => a.slice(a.indexOf(';') + 2))
-  let final = newSplitter[newSplitter.length - 1].indexOf('--' + boundary + '--')
-  newSplitter[newSplitter.length - 1] = newSplitter[newSplitter.length - 1]
-  .slice(0, final)
-  let newData = newSplitter.map(a => a.trim())
-  newData = newData.map(a => a.split('\r\n\r\n'))
-  .reduce((a, v) => {
-    let key = v[0].split('=')[1].split('').filter(a => {
-      if (a !== '"') return a
-    }).join('')
-    let val = v[1]
-    a[key] = val
-    return a
-  }, {})
-  req.body = newData
-  return [req, res]
+const getKeyValue = (data, body) => {
+  let obj = {}
+  let key = data.slice(data.indexOf('=') + 2, data.lastIndexOf(';') - 1)
+  let val = data.slice(data.indexOf('filename') + 10, data.lastIndexOf('"'))
+  obj[key] = Buffer.from(body)
+  return [obj, val]
 }
 
 module.exports = {
